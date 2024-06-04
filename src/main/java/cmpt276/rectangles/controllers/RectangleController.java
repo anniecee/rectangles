35d8ceb9adbb0ab174180ee679d5266be8ccfbc9
Annie <cung.ngananh@gmail.com>
2024-06-04 14:18:55 -0700

    update functions done

--- src/main/java/cmpt276/rectangles/controllers/RectangleController.java
+++ src/main/java/cmpt276/rectangles/controllers/RectangleController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import cmpt276.rectangles.models.Rectangle;
@@ -27,12 +31,24 @@ public class RectangleController {
 
         System.out.println("Getting all rectangles");
         // get all rectangles from database
-        List <Rectangle> rectangles = recRepo.findAll();
+        List <Rectangle> rectangles = recRepo.findAll(Sort.by(Sort.Direction.ASC, "rid"));
         // end of database call
 
         model.addAttribute("recs", rectangles);
 
         return "rectangles/showAll";
+    }
+
+    @GetMapping("/rectangles/view/{rid}")
+    public String getRectangle(Model model, @PathVariable int rid){
+        
+        System.out.println("GET User " + rid);
+
+        // call db
+        Rectangle recTarget = recRepo.findByRid(rid);
+
+        model.addAttribute("rec", recTarget);
+        return "rectangles/showRec";
     }
 
     @PostMapping("/rectangles/add")
@@ -50,4 +66,57 @@ public class RectangleController {
         response.setStatus(201);
         return "rectangles/addedRec";
     }
+
+    @PostMapping("/rectangles/view/{rid}/updatename") 
+    public String updateRecName(Model model, @PathVariable int rid, @RequestParam Map<String, String> recDetails, HttpServletResponse response) { 
+        // Update rectangle record here
+        Rectangle rectangle = recRepo.findByRid(rid);
+
+        rectangle.setName(recDetails.get("name"));
+        
+        recRepo.save(rectangle);
+
+        model.addAttribute("rec", rectangle);
+        return "rectangles/updatedRec";
+    }
+
+    @PostMapping("/rectangles/view/{rid}/updatecolor") 
+    public String updateRecColor(Model model, @PathVariable int rid, @RequestParam Map<String, String> recDetails, HttpServletResponse response) { 
+        // Update rectangle record here
+        Rectangle rectangle = recRepo.findByRid(rid);
+
+        rectangle.setColor(recDetails.get("color"));
+        
+        recRepo.save(rectangle);
+
+        model.addAttribute("rec", rectangle);
+        return "rectangles/updatedRec";
+    }
+
+    @PostMapping("/rectangles/view/{rid}/updatewidth") 
+    public String updateRecWidth(Model model, @PathVariable int rid, @RequestParam Map<String, String> recDetails, HttpServletResponse response) { 
+        // Update rectangle record here
+        Rectangle rectangle = recRepo.findByRid(rid);
+
+        rectangle.setWidth(Integer.parseInt(recDetails.get("width")));
+        
+        recRepo.save(rectangle);
+
+        model.addAttribute("rec", rectangle);
+        return "rectangles/updatedRec";
+    }
+
+    @PostMapping("/rectangles/view/{rid}/updateheight") 
+    public String updateRecHeight(Model model, @PathVariable int rid, @RequestParam Map<String, String> recDetails, HttpServletResponse response) { 
+        // Update rectangle record here
+        Rectangle rectangle = recRepo.findByRid(rid);
+
+        rectangle.setHeight(Integer.parseInt(recDetails.get("height")));
+        
+        recRepo.save(rectangle);
+
+        model.addAttribute("rec", rectangle);
+        return "rectangles/updatedRec";
+    }
+
 }
--- src/main/java/cmpt276/rectangles/models/RectangleRepository.java
+++ src/main/java/cmpt276/rectangles/models/RectangleRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RectangleRepository extends JpaRepository<Rectangle,Integer> {
+    Rectangle findByRid(int rid);
     List<Rectangle> findByName(String name);
     List<Rectangle> findByWidthAndHeight(int width, int height);
 }
